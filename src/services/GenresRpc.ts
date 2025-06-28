b2059fa79650277d95d09ad98703e665d36b2e9f
kpicoder <vladshcheglov41@gmail.com>
2025-06-28 18:24:33 -0400

    feat: genres rpc

---
+++ src/services/GenresRpc.ts
@@ -0,0 +1,36 @@
+import {GenresContract} from "@/interfaces/GenresContract.ts";
+import {Result} from "@/lib/monads/Result";
+import {createConnectTransport} from "@connectrpc/connect-web";
+import {config} from "@/config.ts";
+import {createClient} from "@connectrpc/connect";
+import {GenreService} from "@/gen/app/genres/v1/genres_pb.ts";
+import {genreSchema} from "@/schemas/genre/genre.ts";
+
+export class GenresRpc implements GenresContract {
+    private rpcClient
+
+    constructor() {
+        const transport = createConnectTransport({
+            baseUrl: config.apiBaseUrl
+        })
+        this.rpcClient = createClient(GenreService, transport)
+    }
+
+    async getGenres(): Promise<Result<string[], Error>> {
+        try {
+            const {genres} = await this.rpcClient.getGenres({})
+            const {data, error} = await genreSchema.spa(genres);
+
+            if (error || !data) {
+                return Result.setError(error ?? new Error('Unknown schema error'));
+            }
+
+            return Result.setValue(data);
+        } catch (e) {
+            return Result.setError(e instanceof Error ? e : new Error('Unexpected error'));
+        }
+
+    }
+}
+
+export const genresRpc = new GenresRpc();
