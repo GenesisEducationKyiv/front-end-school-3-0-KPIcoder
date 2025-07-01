24dd308d11cee8f36b18fea46884d7f41bbaaa42
kpicoder <vladshcheglov41@gmail.com>
2025-07-01 19:01:13 -0400

    fix: use json for client streaming

--- src/services/TracksRpc.ts
+++ src/services/TracksRpc.ts
@@ -11,13 +11,19 @@ import {tracksWithMetadataSchema} from "@/schemas/track/tracksWithMetadata.ts";
 export class TracksRpc implements TracksContract {
 
     private rpcClient
+    private rpcJsonClient
 
     constructor() {
         const transport = createConnectTransport({
             baseUrl: config.apiBaseUrl,
             useBinaryFormat: true,
         })
+        const jsonTransport = createConnectTransport({
+            baseUrl: config.apiBaseUrl,
+            useBinaryFormat: false,
+        })
         this.rpcClient = createClient(TrackService, transport);
+        this.rpcJsonClient = createClient(TrackService, jsonTransport);
     }
 
     async getTracks(filterOptions: TracksFilterOptions) {
@@ -60,27 +66,15 @@ export class TracksRpc implements TracksContract {
         return
     }
 
-    async uploadTrackFile (id: string, file: File) {
-         await this.rpcClient.uploadTrackFile(this.prepareStream(id, file));
+    async uploadTrackFile(id: string, file: File) {
+        const buffer = await file.arrayBuffer()
+        const binary = new Uint8Array(buffer)
+        await this.rpcJsonClient.uploadTrackFile({id, fileName: file.name, chunk: binary});
     }
 
-    async deleteTrackFile (id: string){
+    async deleteTrackFile(id: string) {
         await new Promise((r) => setTimeout(() => r(id)));
         throw new Error('Not implemented');
     };
 
-    private async* prepareStream(trackId: string, file: File) {
-        yield {
-            id: trackId,
-            fileName: file.name,
-        };
-
-        const reader = file.stream().getReader();
-
-        while (true) {
-            const { done, value } = await reader.read();
-            if (done) break;
-            yield { chunk: value };
-        }
-    }
 }
